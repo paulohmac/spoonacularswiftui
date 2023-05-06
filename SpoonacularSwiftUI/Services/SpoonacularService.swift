cc26bc6f1e60b218a2ea91775bb20930b4470d58
Paulo H.M <paulo.machtura@vee.digital>
2023-05-05 23:01:39 -0300

    First commit

---
+++ SpoonacularSwiftUI/Services/SpoonacularService.swift
@@ -0,0 +1,13 @@
+//
+//  SpoonacularService.swift
+//  SpoonacularSwiftUI
+//
+//  Created by Paulo H.M. on 05/05/23.
+//
+
+import Foundation
+
+
+
+
+
---
+++ SpoonacularSwiftUI/SpoonacularSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoonacularSwiftUIApp.swift
+//  SpoonacularSwiftUI
+//
+//  Created by Paulo H.M. on 03/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SpoonacularSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
